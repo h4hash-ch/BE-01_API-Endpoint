608fa51e92471d88ab8189d2a427a056e23fd06c
h4hash-ch <h4hashimchoudhry@gmail.com>
2026-07-15 03:25:47 +0500

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -4,21 +4,40 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+const tasks = [{ id: 1, title: "Task 1", done: "True" },
+{ id: 2, title: "Task 2", done: "False" },
+{ id: 3, title: "Task 3", done: "False" }
+];
 
 
-///////////////////Stage 1: Your First Real API Endpoint/////////////////////
+///////////////////Stage 2: List and Single Task/////////////////////
 
-// Endpoint
+// Endpoints
 app.get("/", (req, res) => {
-    res.json({ 
+    res.json({
         name: "Task API",
         version: "Version 1",
         endpoints: ["/tasks"]
-     });
+    });
 });
 
+app.get("/tasks", (req, res) => {
+    res.json(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
+    if (!task) {
+        return res.status(404).json({ error: `Task ${taskId} not found` });
+    }
+    res.json(task);
+});
+
+
+//Health Endpoint (to check server, a professional practice in the industry)
 app.get("/health", (req, res) => {
-    res.json({ 
+    res.json({
         status: "OK"
     });
 });
